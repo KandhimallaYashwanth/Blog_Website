cc4d0740ca0fa7c32ccb6a2a3abcd1cb69aa04d3
KandhimallaYashwanth <kandhimallayashwanth7@gmail.com>
2025-09-19 21:00:43 +0530

    feat(posts): implement fetch single post by id API

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -2,6 +2,9 @@ import express from "express";
 import { createPost } from "../controllers/postController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { createPost, getAllPosts } from "../controllers/postController.js";
+import { getPostById } from "../controllers/postController.js";
+
+
 
 
 
@@ -10,6 +13,7 @@ const router = express.Router();
 // Create post (protected)
 router.post("/", authMiddleware, createPost);
 router.get("/", getAllPosts);
+router.get("/:id", getPostById);
 
 
 export default router;
