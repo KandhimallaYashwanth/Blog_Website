bb5a28c761bdcd230812978e9f0b5db497a8267c
KandhimallaYashwanth <kandhimallayashwanth7@gmail.com>
2025-09-19 21:18:55 +0530

    chore(post): update routes

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,18 +1,12 @@
 import express from "express";
-import { createPost } from "../controllers/postController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { createPost, getAllPosts } from "../controllers/postController.js";
-import { getPostById } from "../controllers/postController.js";
-import { updatePost } from "../controllers/postController.js";
-import { deletePost } from "../controllers/postController.js";
-
-
-
-
-
-
-
-
+import {
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+} from "../controllers/postController.js";
 
 const router = express.Router();
 
@@ -22,7 +16,5 @@ router.get("/", getAllPosts);
 router.get("/:id", getPostById);
 router.put("/:id", authMiddleware, updatePost);
 router.delete("/:id", authMiddleware, deletePost);
-
-
 
 export default router;
